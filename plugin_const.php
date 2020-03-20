fe2b8ce91e11cf0797ac5f2bbe97ea90b1c418b2
Ulibka <gena68@inbox.ru>
2020-03-20 15:27:56 +0300

    initial commit

---
+++ plugin_const.php
@@ -0,0 +1,15 @@
+<?php
+
+class VggGutenConst
+{
+    // файл для трансляции
+    const TRANSLATION_DOMAIN = 'gutenblock_vgg';
+    const THEME_CATEGORY = 'VGG блоки';
+    // const SLUG_THEME_CATEGORY = 'mytheme-category';
+    const SLUG_THEME_CATEGORY = 'vgg-theme-category';
+    // const NAMESPACE = 'mytheme-blocks';
+    const NAMESPACE = 'vgg-lrn-blocks';
+
+    const BLK_NAME_SECOND = "/secondblock";
+    const BLK_NAME_RENDERBLK = "/renderblock";
+}
---
+++ src/constants/index.js
@@ -0,0 +1,16 @@
+class VggGutenConst {
+    static TRANSLATION_DOMAIN = 'gutenblock_vgg';
+    static THEME_CATEGORY = 'VGG блоки';
+    // static NAMESPACE = 'mytheme-blocks';
+    static NAMESPACE = 'vgg-lrn-blocks';
+    // static SLUG_THEME_CATEGORY = 'mytheme-category';
+    static SLUG_THEME_CATEGORY = 'vgg-theme-category';
+
+    static BLK_NAME_TEAM_MEMBERS = "/team-members";
+    static BLK_NAME_TEAM_MEMBER = "/team-member";
+    static BLK_NAME_SECOND = "/secondblock";
+    static BLK_NAME_RENDERBLK = "/renderblock";
+
+}
+
+export default VggGutenConst;

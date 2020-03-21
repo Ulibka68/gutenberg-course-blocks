136803eb844916da820cfab99e7f4d4f68bf2ac8
Gayrat <gena68@inbox.ru>
2020-03-21 16:39:19 +0300

    eslint поправил

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,5 +24,11 @@ module.exports = {
         "react/react-in-jsx-scope": "off",
         "react/display-name": "off",
         "react/prop-types": "off"
+    },
+    "settings": {
+        "react": {
+        "version": "detect"
+        }
     }
+      
 };

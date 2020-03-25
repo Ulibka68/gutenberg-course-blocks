46a785f7324100f69f77347e94efdb07e4ed36e8
Gayrat <gena68@inbox.ru>
2020-03-25 19:21:35 +0300

    корректировка package.json

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
 const autoprefixer = require("autoprefixer");
-const MiniCSSExtractPlugin = require("mini-css-extract-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
-const CleanPlugin = require("clean-webpack-plugin");
+
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = (env, argv) => {
     function isDevelopment() {
@@ -11,7 +12,8 @@ module.exports = (env, argv) => {
     var config = {
         entry: {
             editor: "./src/editor.js",
-            script: "./src/script.js"
+            script: "./src/script.js",
+            editor_script: "./src/editor_script.js"
         },
         output: {
             filename: "[name].js"
@@ -32,15 +34,17 @@ module.exports = (env, argv) => {
             ]
         },
         plugins: [
-            new CleanPlugin(),
-            new MiniCSSExtractPlugin({
-                chunkFilename: "[id].css",
-                filename: chunkData => {
-                    return chunkData.chunk.name === "script"
-                        ? "style.css"
-                        : "[name].css";
+            new CleanWebpackPlugin({
+                // Write Logs to Console
+                verbose: true,
+            }),
+            new MiniCssExtractPlugin({
+                chunkFilename: '[id].css',
+                moduleFilename: (chunk) => {
+                  const { name } = chunk;
+                  return   name === "script" ? "style.css" : "[name].css";
                 }
-            })
+              })
         ],
         devtool: isDevelopment() ? "cheap-module-source-map" : "source-map",
         module: {
@@ -74,7 +78,7 @@ module.exports = (env, argv) => {
                 {
                     test: /\.(sa|sc|c)ss$/,
                     use: [
-                        MiniCSSExtractPlugin.loader,
+                        MiniCssExtractPlugin.loader,
                         "css-loader",
                         {
                             loader: "postcss-loader",
@@ -82,6 +86,7 @@ module.exports = (env, argv) => {
                                 plugins: [autoprefixer()]
                             }
                         },
+                     
                         "sass-loader"
                     ]
                 }
@@ -89,11 +94,18 @@ module.exports = (env, argv) => {
         },
         externals: {
             jquery: "jQuery",
+            lodash: "lodash",
             "@wordpress/blocks": ["wp", "blocks"],
             "@wordpress/i18n": ["wp", "i18n"],
             "@wordpress/editor": ["wp", "editor"],
             "@wordpress/components": ["wp", "components"],
-            "@wordpress/element": ["wp", "element"]
+            "@wordpress/element": ["wp", "element"],
+            "@wordpress/blob": ["wp", "blob"],
+            "@wordpress/data": ["wp", "data"],
+            "@wordpress/html-entities": ["wp", "htmlEntities"],
+            "@wordpress/compose": ["wp", "compose"],
+            "@wordpress/plugins": ["wp", "plugins"],
+            "@wordpress/edit-post": ["wp", "editPost"]
         }
     };
     return config;

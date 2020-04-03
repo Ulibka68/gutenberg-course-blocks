5786d5c864ce2a0c8ed406380226cd6cee2501d0
Gayrat <gena68@inbox.ru>
2020-04-03 09:05:00 +0300

    todo meta initial commit

--- plugin.php
+++ plugin.php
@@ -11,7 +11,7 @@ if ( ! defined( 'ABSPATH' )) {
     exit;
 }
 
-include_once('src/metabox.php');
+include_once('src/phpcode/metabox.php');
 include_once('src/phpcode/plugin_const.php');
 include_once('src/phpcode/register-cust.php');
 
